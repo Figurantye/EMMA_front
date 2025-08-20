c2c081ee25b15ab76884aa70affe2bc38078ebb5
Gustavo Wazlawick <143718452+Figurantye@users.noreply.github.com>
2025-08-20 15:44:05 -0300

    traduzindo e atualizando uri de redirecionamento para autenticação com google

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -3,25 +3,26 @@ import styles from './Auth.module.css';
 
 const Login: React.FC = () => {
     const handleGoogleLogin = () => {
-        window.location.href = 'http://emmaback-production.up.railway.app/auth/google';
+        window.location.href = 'https://emmaback-production.up.railway.app/api/auth/google';
     };
 
     return (
         <div className={styles.authContainer}>
             <div className={styles.card}>
-                <h2>Bem-vindo ao EMMA</h2>
+                <h2>Welcome to EMMA</h2>
 
                 <div style={{ justifyItems: 'center' }}>
                     <div style={{height:30}}></div>
                     <button className={styles.googleButton} style={{ backgroundColor: 'blue', color: 'white' }} onClick={handleGoogleLogin}>
-                        Entrar com Google
+                        Login with Google
                     </button>
-                    <p className={styles.subtitle} style={{margin: 20}}>Faça login para continuar</p>
+                    <p className={styles.subtitle} style={{margin: 20}}>Log in to continue</p>
                 </div>
 
             </div>
         </div>
     );
 };
+
 
 export default Login;
